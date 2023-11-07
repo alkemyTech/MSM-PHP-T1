ee68776d022318f858c88cb533d74a3d82c4f248
javiercavalero <85362104+javiercavalero@users.noreply.github.com>
2023-11-07 20:23:52 -0300

    SMP1-9 modelo y migracion de fixedTerm

---
+++ app/Models/FixedTerm.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class FixedTerm extends Model
+{
+    use HasFactory;
+
+    //asigna qué columnas de la base de datos se pueden llenar de forma masiva
+    protected $fillable = [
+        'amount',
+        'interest',
+        'total',
+        'duration'
+    ];
+
+    //oculta los siguientes campos para que no aparezcan al hacer una consulta a la base de datos
+    protected $hidden = [
+        'account_id',
+        'created_at',
+        'updated_at',
+        'closed_at'
+    ];
+
+    public function account(): BelongsTo
+    {
+        return $this->belongsTo(Account::class);
+    }
+
+}
---
+++ database/migrations/2023_11_07_140528_fixed_terms.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // funcion para crear las tablas en la base de datos 
+        Schema::create('fixed_terms', function (Blueprint $table) {
+            $table->id(); 
+            $table->double('amount'); 
+            $table->foreign('account_id')->references('id')->on('accounts'); //vinculacion con la tabla account
+            $table->double('interest');
+            $table->double('total');
+            $table->integer('duration');
+            $table->timestamp('closed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fixed_terms'); //funcion para eliminar la tabla fixed_terms si es necesario
+    }
+};
