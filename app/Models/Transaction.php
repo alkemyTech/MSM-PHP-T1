acefac3e99c277e068cdc252ad11b91cb1f99ef7
Emilianordz <rodriguezeme93@gmail.com>
2023-11-07 17:36:20 -0300

    SMP1-8 Creacion modelo Transaction + migracion

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -10,11 +10,11 @@ class Transaction extends Model //Creacion de la nueva clase "Transaction"
     use HasFactory;
 
     protected $fillable  = [ //Configuracion de las propiedades rellenables en base de datos
-        'amount',
-        'type',
-        'description',
-        'account_id',
-        'transaction_date',
+        'amount', //Monto de la transaccion
+        'type', //Tipo de transaccion (INCOME, PAYMENT, DEPOSIT)
+        'description', //Descripcion de la operacion (puede ser valor nulo)
+        'account_id', //Clave foranea hacia ID de tabla "account" correspondiente a la cuenta responsable de la operacion
+        'transaction_date', //Registro de fecha y hora en que fue realizada la transaccion
     ];
 
     public function account() //Esta funcion indica que nuestro modelo "transaction" pertenece a "account" y asocia a los datos de la transaccion a la cuenta del cliente.
