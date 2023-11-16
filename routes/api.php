bdb1f87589121ef81aa7243688cacb995974a0d8
Fabian <fabianloza.fgl@gmail.com>
2023-11-16 19:48:07 -0300

    SMP1-20 Endpoint para realizar un depósito

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
 
 
@@ -29,9 +30,12 @@ Route::middleware(['api', 'auth:api'])->group(function () {
         // RUTA LOGIN: Esta ruta maneja la autenticacion de usuarios.
         Route::post('login', [AuthController::class, 'login'])->name('auth.login')->withoutMiddleware(['auth:api']);
 
-        // SOLICITUD DELETE A /users/{id} 
+        // SOLICITUD DELETE A /users/{id} para eliminar un usuario
         Route::delete('/users/{id}', [UserController::class, 'delete']);
     });
-        // SOLICITUD POST a /accounts para la creación de cuentas
+        // RUTA A /accounts: Esta ruta maneja la creación de cuentas
         Route::post('/accounts', [AccountController::class, 'createAccount']);
+
+        // RUTA A /transactions/deposit: Esta ruta maneja el depósito en una cuenta propia.
+        Route::post('/transactions/deposit', [TransactionController::class, 'deposit']);
 });
