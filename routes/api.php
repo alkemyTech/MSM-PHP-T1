ea3e6e55c4189bce4927b27f5850c5da593f3a18
Fabian <fabianloza.fgl@gmail.com>
2023-11-21 21:22:59 -0300

    SMP1-18 Endpoint para enviar dinero

--- routes/api.php
+++ routes/api.php
@@ -33,16 +33,16 @@ Route::middleware(['api', 'auth:api'])->group(function () {
         // SOLICITUD DELETE A /users/{id} para eliminar un usuario
         Route::delete('/users/{id}', [UserController::class, 'delete']);
       
+    });
         // RUTA A /accounts: Esta ruta maneja la creación de cuentas
-        Route::post('/accounts', [AccountController::class, 'createAccount']);
-
-        // SOLICITUD POST a /accounts para la creación de cuentas
         Route::post('/accounts', [AccountController::class, 'createAccount']);
 
         // SOLICITUD GET A /users  para traer todos los usuarios 
         Route::get('/users', [UserController::class, 'index']);
-      
+    
         // RUTA A /transactions/deposit: Esta ruta maneja el depósito en una cuenta propia.
         Route::post('/transactions/deposit', [TransactionController::class, 'deposit']);
-    });
+
+        // RUTA A /transactions/send: Esta ruta maneja el envio de dinero entre cuentas
+        Route::post('/transactions/send', [TransactionController::class, 'sendMoney']);
 });
